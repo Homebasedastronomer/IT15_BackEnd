d88b77241b48fcec8cb097acd38b618cba106ebe
Mark Kian Dollente <m.dollente.137510.tc@umindanao.edu.ph>
2026-02-10 22:08:56 +0800

    second upload

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('dashboard', function () {
+Route::get('/dashboard', function () {
     return Inertia::render('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
+Route::get('/welcome', function () {
+    return Inertia::render('welcome');
+})->name('welcome');
+
+require __DIR__ . '/settings.php';
